361ea27bf161efe9051606b856e831a3bdbe35e9
Jones Martins <jonesmvc@hotmail.com>
2016-11-15 18:34:47 -0200

    No more dictionary for YOU!

--- vaporwave.py
+++ vaporwave.py
@@ -1,49 +1,36 @@
 import pyperclip
 import sys
 
-"""
-Using a dictionary is not very efficient, but is predictable.
-"""
 def vaporwave(sentence):
-    character_dict = {"!": "！", "#": "＃", "$": "＄", "%": "％", "&": "＆", "'": "＇", "(": "（",
-                  ")": "）", "*": "＊", "+": "＋", ",": "，", "-": "－", ".": "．", "/": "／",
-                  "0": "０", "1": "１", "2": "２", "3": "３", "4": "４", "5": "５", "6": "６",
-                  "7": "７", "8": "８", "9": "９", ":": "：", ";": "；", "<": "＜", "=": "＝",
-                  ">": "＞", "?": "？", "@": "＠", "A": "Ａ", "B": "Ｂ", "C": "Ｃ", "D": "Ｄ",
-                  "E": "Ｅ", "F": "Ｆ", "G": "Ｇ", "H": "Ｈ", "I": "Ｉ", "J": "Ｊ", "K": "Ｋ",
-                  "L": "Ｌ", "M": "Ｍ", "N": "Ｎ", "O": "Ｏ", "P": "Ｐ", "Q": "Ｑ", "R": "Ｒ",
-                  "S": "Ｓ", "T": "Ｔ", "U": "Ｕ", "V": "Ｖ", "W": "Ｗ", "X": "Ｘ", "Y": "Ｙ",
-                  "Z": "Ｚ", "[": "［","\\": "＼", "]": "］", "^": "＾", "_": "＿", "`": "｀",
-                  "a": "ａ", "b": "ｂ", "c": "ｃ", "d": "ｄ", "e": "ｅ", "f": "ｆ", "g": "ｇ",
-                  "h": "ｈ", "i": "ｉ", "j": "ｊ", "k": "ｋ", "l": "ｌ", "m": "ｍ", "n": "ｎ",
-                  "o": "ｏ", "p": "ｐ", "q": "ｑ", "r": "ｒ", "s": "ｓ", "t": "ｔ", "u": "ｕ",
-                  "v": "ｖ", "w": "ｗ", "x": "ｘ", "y": "ｙ", "z": "ｚ", "{": "｛", "|": "｜",
-                  "}": "｝", "~": "～"}
     new_sentence = ""
     for character in sentence:
-        if character in character_dict:
-            new_sentence += character_dict[character] + " "
+        ord_char = ord(character)
+        if ord_char >= 33 and ord_char <= 127:
+            new_sentence += chr(ord_char + 65248) + " "
         else:
             new_sentence += character + " "
-
+            
     pyperclip.copy(new_sentence)
     print("Result in your clipboard.")
 
 
 def parse_command(text):
     command = text[:6]
+    string = text[6:]
     if command == "upper ":
-        vaporwave(text[6:].upper())
+        string = string.upper()
     elif command == "lower ":
-        vaporwave(text[6:].lower())
+        string = string.lower()
     elif command == "title ":
-        vaporwave(text[6:].title())
+        string = string.title()
     elif text == "":
         print("Exiting.")
         sys.exit() #breaks while loop
     else:
         print("Input Error.")
-
+        sys.exit() #stops program
+        
+    transform_vaporwave(string) 
 
 def main(argv):
     length = len(argv)
