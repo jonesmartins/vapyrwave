637a417c25117ee9db38ccdce33041b17153a4c2
Jones Martins <jonesmvc@hotmail.com>
2017-01-22 23:07:11 -0200

    Add files via upload

--- vaporwave.py
+++ vaporwave.py
@@ -1,19 +1,42 @@
-import sys, pyperclip
+import pyperclip
+import sys
+import argparse
+
+def read_argv(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sentence', type=str, help='Sentence to transform')
+    parser.add_argument('-v', '--vertical', dest='vertical', action='store_true',
+                        help='Prints result in vertical and horizontal manner')
+    return parser.parse_args(argv)
+
+def check_cmd(cmd):
+    try:
+        return vars(cmd)
+    except TypeError:
+        return cmd
 
 def transform_vaporwave(sentence):
     new_sentence = ""
     for character in sentence:
         ord_char = ord(character)
-        if ord_char >= 33 and ord_char <= 127:
-            new_sentence += chr(ord_char + 65248) + " "    
-        else:
-            new_sentence += character + " "
+        if 33 <= ord_char <= 127:
+            character = chr(ord_char + 65248)
+        new_sentence += character + " "
+    return new_sentence.rstrip()
 
-    pyperclip.copy(new_sentence)  # sends result to clipboard
+def add_vertical(sentence):
+    final_sentence = "{}\n".format(sentence)
+    for pos in range(2, len(sentence), 2):
+        final_sentence += "{}\n".format(sentence[pos])
+    return final_sentence.rstrip('\n')
+
+def send_to_clipboard(sentence):
+    pyperclip.copy(sentence)  # remove last space
     print("Result in your clipboard.")
-
+    
+'''
 def parse_cmd(text):
-    command = text[:6].lower() 
+    command = text[:6].lower()
     if command == "upper ":
         string = text[6:].upper()
     elif command == "lower ":
@@ -21,22 +44,22 @@ def parse_cmd(text):
     elif command == "title ":
         string = text[6:].title()
     elif text == "":
-        sys.exit("Exiting.")  # stops while loop
+        print("Exiting.")
+        sys.exit() #stops while loop
     else:
-        sys.exit("Input Error.")  # stops program
-        
+        print("Input Error.")
+        sys.exit() #stops program
+
     transform_vaporwave(string)
-    
+'''
 
-def main(argv):
-    if len(argv) == 1:  # only filename
-        while True:
-            sentence = input("Sentence: ")
-            parse_cmd(sentence)
-    else:
-        sentence = " ".join(argv[1:])
-        parse_cmd(sentence)    
-    
+def main():
+    cmd = read_argv(sys.argv[1:])
+    cmd_dict = check_cmd(cmd)
+    sentence = transform_vaporwave(cmd_dict['sentence'])
+    if cmd_dict['vertical']:
+        sentence = add_vertical(sentence)
+    send_to_clipboard(sentence)
+
 if __name__ == '__main__':
-    main(sys.argv)
-
+    main()
