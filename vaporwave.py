da6b72c86009be56ff419f770c5a9ad9d3c62f7d
Jones Martins <jonesmvc@ufrj.br>
2017-03-06 19:13:36 -0300

    Add more options

--- vaporwave.py
+++ vaporwave.py
@@ -1,13 +1,18 @@
 import pyperclip
-import sys
 import argparse
+import sys
 
 def read_argv(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('sentence', type=str, help='Sentence to transform')
-    parser.add_argument('-v', '--vertical', dest='vertical', action='store_true',
-                        help='Prints result in vertical and horizontal manner')
+    parser.add_argument('sentence', help='Sentence to transform')
+    parser.add_argument('-v', dest='vertical', action='store_true',
+                        help='Prints result in vertical manner')
+    parser.add_argument('-s', dest='spaced', action='store_true',
+                        help='Prints result with spaces amidst letters')
+    parser.add_argument('-d', dest='double', action='store_true',
+                        help='Makes big square of letters')
     return parser.parse_args(argv)
+
 
 def check_cmd(cmd):
     try:
@@ -15,51 +20,57 @@ def check_cmd(cmd):
     except TypeError:
         return cmd
 
+
 def transform_vaporwave(sentence):
-    new_sentence = ""
+    new_sentence = ''
+    char_distance = ord('Ａ') - ord('A')  # 65248
     for character in sentence:
         ord_char = ord(character)
         if 33 <= ord_char <= 127:
-            character = chr(ord_char + 65248)
-        new_sentence += character + " "
-    return new_sentence.rstrip()
+            character = chr(ord_char + char_distance)
+        new_sentence += character
+    return new_sentence
 
-def add_vertical(sentence):
-    final_sentence = "{}\n".format(sentence)
-    for pos in range(2, len(sentence), 2):
-        final_sentence += "{}\n".format(sentence[pos])
+
+def make_vertical(sentence):
+    new_sentence = ''
+    for letter in sentence:
+        new_sentence += '{}\n'.format(letter)
+    return new_sentence.rstrip('\n')
+
+
+def make_horizontal(sentence):
+    new_sentence = ''
+    for letter in sentence:
+        new_sentence += '{} '.format(letter)
+    return new_sentence
+
+
+def make_both(sentence):
+    final_sentence = '{}\n'.format(make_horizontal(sentence))
+    for pos in range(1, len(sentence)):
+        final_sentence += '{}\n'.format(sentence[pos])
     return final_sentence.rstrip('\n')
 
+   
 def send_to_clipboard(sentence):
-    pyperclip.copy(sentence)  # remove last space
+    pyperclip.copy(sentence)
     print("Result in your clipboard.")
     
-'''
-def parse_cmd(text):
-    command = text[:6].lower()
-    if command == "upper ":
-        string = text[6:].upper()
-    elif command == "lower ":
-        string = text[6:].lower()
-    elif command == "title ":
-        string = text[6:].title()
-    elif text == "":
-        print("Exiting.")
-        sys.exit() #stops while loop
-    else:
-        print("Input Error.")
-        sys.exit() #stops program
-
-    transform_vaporwave(string)
-'''
-
-def main():
-    cmd = read_argv(sys.argv[1:])
-    cmd_dict = check_cmd(cmd)
+def main(argv):
+    cmd = read_argv(argv)
+    cmd_dict = check_cmd(cmd)    
     sentence = transform_vaporwave(cmd_dict['sentence'])
-    if cmd_dict['vertical']:
-        sentence = add_vertical(sentence)
+    if cmd_dict['vertical'] and cmd_dict['spaced'] and not cmd_dict['double']:
+        sentence = make_both(sentence)        
+    elif cmd_dict['vertical'] and not cmd_dict['double']:
+        sentence = make_vertical(sentence)
+    elif cmd_dict['spaced'] and not cmd_dict['double']:
+        sentence = make_horizontal(sentence)
+    elif cmd_dict['double']:
+        sentence = make_double(sentence)
     send_to_clipboard(sentence)
-
+    
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
+
