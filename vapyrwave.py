fe37c23f8bb2668b3e78497ca14dce2141aecfa9
Jones Martins <jonesmvc@ufrj.br>
2017-04-02 18:35:25 -0300

    Fix spaces

--- vapyrwave.py
+++ vapyrwave.py
@@ -2,13 +2,14 @@ import pyperclip
 import argparse
 import sys
 
+
 def read_argv(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('sentence', help='Sentence to transform')
     parser.add_argument('-v', dest='vertical', action='store_true',
-                        help='Prints result in vertical manner')  
+                        help='Prints result in vertical manner')
     parser.add_argument('-s', dest='spaced', default=0, type=int,
-                        help='Prints result with N spaces amidst letters')
+                        help='Prints result with spaces amidst letters')
     return parser.parse_args(argv)
 
 
@@ -50,7 +51,7 @@ def make_both(sentence, spaces):
         final_sentence += '{}\n'.format(sentence[pos])
     return final_sentence.rstrip('\n')
 
-   
+
 def send_to_clipboard(sentence):
     pyperclip.copy(sentence)
     print("Result in your clipboard.")
@@ -60,14 +61,13 @@ def main(argv):
     cmd = read_argv(argv)
     cmd_dict = check_cmd(cmd)    
     sentence = transform_vaporwave(cmd_dict['sentence'])
-    if cmd_dict['vertical'] and cmd_dict['spaced']:
+    if cmd_dict['vertical'] and cmd_dict['spaced'] is not None:
         sentence = make_both(sentence, cmd_dict['spaced'])
-    elif cmd_dict['spaced']:
+    elif cmd_dict['spaced'] is not None:
         sentence = make_horizontal(sentence, cmd_dict['spaced'])
     elif cmd_dict['vertical']:
         sentence = make_vertical(sentence)
     
     send_to_clipboard(sentence)
-    
 if __name__ == '__main__':
     main(sys.argv[1:])
